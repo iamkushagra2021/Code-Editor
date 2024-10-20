2b2d6642f2fb8cdd393f071937333cbcaa1a7990
Kushagra Singh <iamkushagra2021@gmail.com>
2024-10-20 16:29:00 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,33 @@
+let htmlinput=document.querySelector(".html-editor textarea");
+let cssinput=document.querySelector(".css-editor textarea");
+let jsinput=document.querySelector(".js-editor textarea");
+let save=document.querySelector("#save");
+let output=document.querySelector("#output");
+let outputContainer=document.querySelector(".output-container");
+let full=document.querySelector("#full");
+let copy=document.querySelectorAll(".copy");
+
+save.addEventListener("click",()=>{
+    output.contentDocument.body.innerHTML=htmlinput.value;
+    output.contentDocument.head.innerHTML=`<style> ${cssinput.value} </style>`
+    output.contentWindow.eval(jsinput.value);
+ });
+ full.addEventListener("click",()=>{
+outputContainer.classList.toggle("output-full-active")
+ });
+
+ copy.forEach((e)=>{
+    e.addEventListener("click",()=>{
+        if(e.classList.contains("copy1")){
+            navigator.clipboard.writeText(htmlinput.value)
+        }
+        else if(e.classList.contains("copy2")){
+            navigator.clipboard.writeText(cssinput.value)
+        }
+        else{
+            navigator.clipboard.writeText(jsinput.value)
+        }
+    })
+    
+})
+
